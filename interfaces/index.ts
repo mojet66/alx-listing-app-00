22e3f2c7beaa2cd9b8beec5b065adec088ebd351
mojet66 <oladosuemmanuel2017@gmail.com>
2025-06-01 17:40:35 +0100

    Build Responsive Listing Page with Tailwind CSS

--- interfaces/index.ts
+++ interfaces/index.ts
@@ -10,3 +10,22 @@ export interface ButtonProps {
   onClick: () => void;
   disabled?: boolean;
 }
+
+export interface PropertyProps {
+  name: string;
+  address: {
+    state: string;
+    city: string;
+    country: string;
+  };
+  rating: number;
+  category: string[];
+  price: number;
+  offers: {
+    bed: string;
+    shower: string;
+    occupants: string;
+  };
+  image: string;
+  discount: string;
+}
